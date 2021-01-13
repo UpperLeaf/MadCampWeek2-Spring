c4b1f48e207be6cc13c3babeff533df544b64e27
UpperLeaf <tkdduq8260@gmail.com>
2021-01-13 11:22:59 +0900

    Feat : Post Length 조절

--- src/main/java/com/example/demo/blog/Post.java
+++ src/main/java/com/example/demo/blog/Post.java
@@ -2,10 +2,7 @@ package com.example.demo.blog;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @EqualsAndHashCode(of = "id") @NoArgsConstructor
 @Entity @Getter @Setter @AllArgsConstructor
@@ -16,6 +13,7 @@ public class Post {
 
     private String title;
 
+    @Column(length = 500)
     private String content;
 
     @ManyToOne
